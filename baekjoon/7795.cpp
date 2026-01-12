11179cd7904975572d8a2f2d92d3b074e2a3bacc
hyungjunn <hotfirebulldog@gmail.com>
2026-01-12 21:08:12 +0900

    백준 7795번: 세모

---
+++ baekjoon/7795.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int T;
+  cin >> T;
+
+  while (T--) {
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(N), B(M);
+    for (int i = 0; i < N; i++) {
+      cin >> A[i];
+    }
+    for (int i = 0; i < M; i++) {
+      cin >> B[i];
+    }
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    long long count = 0;
+    for (int i = 0; i < N; i++) {
+      auto lower = lower_bound(B.begin(), B.end(), A[i]);
+      count += (lower - B.begin());
+    }
+
+    cout << count << '\n';
+  }
+
+  return 0;
+}
