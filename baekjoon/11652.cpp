3b7497a428936894688b622a8534b4863dacd682
hyungjunn <hotfirebulldog@gmail.com>
2026-01-12 20:09:15 +0900

    백준 11652번: 세모

---
+++ baekjoon/11652.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  vector<long long> v(N);
+  for (int i = 0; i < N; i++) {
+    cin >> v[i];
+  }
+
+  sort(v.begin(), v.end());
+
+  long long ans = v[0];
+  int max_count = 1;
+  int cur_count = 1;
+
+  for (int i = 1; i < N; i++) {
+    if (v[i] == v[i - 1]) {
+      cur_count++;
+    } else {
+      cur_count = 1;
+    }
+    if (cur_count > max_count) {
+      max_count = cur_count;
+      ans = v[i];
+    }
+  }
+
+  cout << ans;
+
+  return 0;
+}
