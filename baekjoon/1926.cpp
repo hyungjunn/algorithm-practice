fe351a9df13b508dc2dbeb12b57e9ae2cb8d40dd
hyungjunn <hotfirebulldog@gmail.com>
2026-01-31 09:31:15 +0900

    백준 1926번: 복습2

--- baekjoon/1926.cpp
+++ baekjoon/1926.cpp
@@ -4,13 +4,13 @@
 #include <queue>
 using namespace std;
 int n, m;
-constexpr int MAX = 505;
-int board[MAX][MAX];
-bool visited[MAX][MAX];
+int board[502][502];
+bool vis[502][502];
 #define X first
 #define Y second
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -18,31 +18,31 @@ int main() {
   for (int i = 0; i < n; i++)
     for (int j = 0; j < m; j++)
       cin >> board[i][j];
-  int pic_count = 0;
-  int max_area = 0;
+  int num = 0;
+  int mx;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      if (visited[i][j] || board[i][j] == 0) continue;
-      // 방문을 하지 않았고 1인경우
+      if (vis[i][j] || board[i][j] == 0) continue;
       queue<pair<int, int>> Q;
       Q.emplace(i, j);
-      pic_count++;
-      visited[i][j] = true;
-      int area = 1;
+      num++;
+      vis[i][j] = true;
+      int area = 0;
       while (!Q.empty()) {
         auto cur = Q.front(); Q.pop();
+        area++;
         for (int dir = 0; dir < 4; dir++) {
           int nx = cur.X + dx[dir];
           int ny = cur.Y + dy[dir];
           if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-          if (visited[nx][ny] || board[nx][ny] == 0) continue;
+          if (board[nx][ny] == 0 || vis[nx][ny]) continue;
           Q.emplace(nx, ny);
-          visited[nx][ny] = true;
-          area++;
+          vis[nx][ny] = true;
         }
       }
-      max_area = max(max_area, area);
+      mx = max(mx, area);
     }
   }
-  cout << pic_count << '\n' << max_area;
+  cout << num << '\n' << mx;
+  return 0;
 }
