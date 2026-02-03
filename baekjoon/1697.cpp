8cb1df96b7193153ecfc8aa2653f48d350118417
hyungjunn <hotfirebulldog@gmail.com>
2026-02-03 23:37:13 +0900

    백준 1697번: 엑스

--- baekjoon/1697.cpp
+++ baekjoon/1697.cpp
@@ -10,17 +10,17 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cin >> N >> K;
-  fill(times, times+100004, -1); // TODO: 범위 조심
+  fill(times, times + 100004, -1);
   times[N] = 0;
   queue<int> Q;
-  Q.push(N);
+  Q.emplace(N);
   while (times[K] == -1) {
     auto cur = Q.front(); Q.pop();
-    for (int next : {cur-1, cur+1, cur*2}) { // TODO: 아이디어
-      if (next < 0 || next > 100000) continue;
-      if (times[next] != -1) continue;
-      times[next] = times[cur] + 1;
-      Q.push(next);
+    for (int nxt : {cur-1, cur+1, cur*2}) {
+      if (nxt < 0 || nxt > 100000) continue;
+      if (times[nxt] != -1) continue;
+      times[nxt] = times[cur] + 1;
+      Q.emplace(nxt);
     }
   }
   cout << times[K];
