8840a5e1b9ac5ee2dc818b62fc5ca98669742a31
hyungjunn <hotfirebulldog@gmail.com>
2026-02-03 10:05:44 +0900

    백준 2178번: 복습1

--- baekjoon/2178.cpp
+++ baekjoon/2178.cpp
@@ -3,38 +3,37 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int n, m;
-constexpr int MAX = 105;
-int board[MAX][MAX];
-int visited[MAX][MAX];
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int n, m; // 2 <= n, m <= 100
+char board[102][102];
+bool vis[102][102];
 #define X first
 #define Y second
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, -1, 0, 1};
+int dist[102][102];
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cin >> n >> m;
-      char c;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) {
-      cin >> c;
-      board[i][j] = c - '0';
-    }
-  }
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++)
+      cin >> board[i][j];
   queue<pair<int, int>> Q;
   Q.emplace(1, 1);
-  visited[1][1] = 1;
+  vis[1][1] = true;
+  dist[1][1] = 1;
   while (!Q.empty()) {
     auto cur = Q.front(); Q.pop();
     for (int dir = 0; dir < 4; dir++) {
-      int x = cur.X + dx[dir];
-      int y = cur.Y + dy[dir];
-      if (x < 1 || y < 1 || x > n || y > m) continue;
-      if (visited[x][y] || board[x][y] == 0) continue;
-      Q.emplace(x, y);
-      visited[x][y] = visited[cur.X][cur.Y] + 1;
-      if (x == n && y == m) cout << visited[x][y];
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if (nx < 1 || ny < 1 || nx > n || ny > m) continue;
+      if (vis[nx][ny] || board[nx][ny] == '0') continue;
+      vis[nx][ny] = true;
+      dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+      Q.emplace(nx, ny);
     }
   }
+  cout << dist[n][m];
+  return 0;
 }
