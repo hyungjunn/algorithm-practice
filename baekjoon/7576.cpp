1119224073de5096dc42fa456dfd2bd46ec7d76e
hyungjunn <hotfirebulldog@gmail.com>
2026-02-03 11:04:18 +0900

    백준 7576번: 복습1(한번 더 풀 필요있음)

--- baekjoon/7576.cpp
+++ baekjoon/7576.cpp
@@ -2,56 +2,52 @@
 // https://www.acmicpc.net/problem/7576
 #include <iostream>
 #include <queue>
-using namespace std;
-int M, N;
-constexpr int MAX = 1000;
-int board[MAX][MAX];
-bool visited[MAX][MAX];
+#include <algorithm>
+int n, m;
+int board[1002][1002];
+int dist[1002][1002];
 #define X first
 #define Y second
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int level[MAX][MAX];
-
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, -1, 0, 1};
+using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  cin >> M >> N; // M:가로, N:세로
-  for (int j = 0; j < N; j++)
-    for (int i = 0; i < M; i++)
+  cin >> m >> n;
+  queue<pair<int, int>> Q;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       cin >> board[i][j];
-  queue<pair<int, int> > Q;
-  for (int j = 0; j < N; j++) {
-    for (int i = 0; i < M; i++) {
-      if (visited[i][j] || board[i][j] != 1) continue;
-      Q.emplace(i, j);
-      visited[i][j] = true;
-      level[i][j] = 0;
+      if (board[i][j] == 1) {
+        Q.emplace(i, j);
+      }
+      if (board[i][j] == 0) {
+        dist[i][j] = -1;
+      }
     }
   }
   while (!Q.empty()) {
-    auto cur = Q.front();
-    Q.pop();
+    auto cur = Q.front(); Q.pop();
     for (int dir = 0; dir < 4; dir++) {
-      int x = cur.X + dx[dir];
-      int y = cur.Y + dy[dir];
-      if (x < 0 || y < 0 || x >= M || y >= N) continue;
-      if (visited[x][y] || board[x][y] == -1) continue;
-      Q.emplace(x, y);
-      board[x][y] = 1;
-      visited[x][y] = true;
-      level[x][y] = level[cur.X][cur.Y] + 1;
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+      if (dist[nx][ny] >= 0) continue;
+      dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+      Q.emplace(nx, ny);
     }
   }
-  int max_days = 0;
-  for (int j = 0; j < N; j++) {
-    for (int i = 0; i < M; i++) {
-      if (board[i][j] == 0) {
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (dist[i][j] == -1) {
         cout << -1;
         return 0;
       }
-      max_days = max(max_days, level[i][j]);
+      ans = max(ans, dist[i][j]);
     }
   }
-  cout << max_days;
+  cout << ans;
+  return 0;
 }
