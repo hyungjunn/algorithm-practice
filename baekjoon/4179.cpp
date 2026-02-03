abad8b0c6a225d2e3d11fe82036d4be7db14ed91
hyungjunn <hotfirebulldog@gmail.com>
2026-02-03 16:31:44 +0900

    백준 4179번: 복습1(한번 더 풀 필요있음)

--- baekjoon/4179.cpp
+++ baekjoon/4179.cpp
@@ -1,74 +1,67 @@
 // 백준 4179번: 불!
 // https://www.acmicpc.net/problem/4179
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <queue>
 using namespace std;
-int R, C; // 1<=R,C<=1000
-constexpr int MAX = 1005;
-char board[MAX][MAX];
-int dist_fire[MAX][MAX];
-int dist_jihun[MAX][MAX];
+
+int R, C; // 1 <= R, C <= 1,000
+string board[1002];
+int dist1[1002][1002]; // 불의 전파 시간
+int dist2[1002][1002]; // 지훈이 이동 시간
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 #define X first
 #define Y second
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
   cin >> R >> C;
   for (int i = 0; i < R; i++) {
-    fill(dist_fire[i], dist_fire[i] + C, -1);
-    fill(dist_jihun[i], dist_jihun[i] + C, -1);
+    fill(dist1[i], dist1[i] + C, -1);
+    fill(dist2[i], dist2[i] + C, -1);
   }
-
-  // 불이 처음 존재하는 곳은 시간을 0으로 설정하고 Q에 넣는다.
-  queue<pair<int, int>> Q_fire;
-  queue<pair<int, int>> Q_jihun;
+  for (int i = 0; i < R; i++)
+    cin >> board[i];
+  queue<pair<int, int>> Q1; // 불
+  queue<pair<int, int>> Q2; // 지훈이
   for (int i = 0; i < R; i++) {
     for (int j = 0; j < C; j++) {
-      cin >> board[i][j];
       if (board[i][j] == 'F') {
-        dist_fire[i][j] = 0;
-        Q_fire.emplace(i, j);
+        Q1.emplace(i, j);
+        dist1[i][j] = 0;
       }
       if (board[i][j] == 'J') {
-        dist_jihun[i][j] = 0;
-        Q_jihun.emplace(i, j);
+        Q2.emplace(i, j);
+        dist2[i][j] = 0;
       }
     }
   }
-
-  // 불 BFS
-  while (!Q_fire.empty()) {
-    auto cur = Q_fire.front(); Q_fire.pop();
+  while (!Q1.empty()) {
+    auto cur = Q1.front(); Q1.pop();
     for (int dir = 0; dir < 4; dir++) {
       int nx = cur.X + dx[dir];
       int ny = cur.Y + dy[dir];
-      if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
-      if (dist_fire[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-      dist_fire[nx][ny] = dist_fire[cur.X][cur.Y] + 1;
-      Q_fire.emplace(nx, ny);
+      if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+      if (dist1[nx][ny] >= 0 || board[nx][ny] == '#') continue;
+      dist1[nx][ny] = dist1[cur.X][cur.Y] + 1;
+      Q1.emplace(nx, ny);
     }
   }
-
-  // 지훈이 BFS
-  while (!Q_jihun.empty()) {
-    auto cur = Q_jihun.front(); Q_jihun.pop();
+  while (!Q2.empty()) {
+    auto cur = Q2.front(); Q2.pop();
     for (int dir = 0; dir < 4; dir++) {
       int nx = cur.X + dx[dir];
       int ny = cur.Y + dy[dir];
-      if (nx < 0 || ny < 0 || nx >= R || ny >= C) {
-        cout << (dist_jihun[cur.X][cur.Y] + 1);
+      if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+        cout << dist2[cur.X][cur.Y] + 1;
         return 0;
       }
-      if (dist_jihun[nx][ny] >= 0 || board[nx][ny] == '#') continue;
-      if (dist_fire[nx][ny] == -1 || dist_jihun[cur.X][cur.Y] + 1 < dist_fire[nx][ny]) {
-        dist_jihun[nx][ny] = dist_jihun[cur.X][cur.Y] + 1;
-        Q_jihun.emplace(nx, ny);
-      }
+      if (dist2[nx][ny] >= 0 || board[nx][ny] == '#') continue;
+      if (dist1[nx][ny] != -1 && dist2[cur.X][cur.Y] + 1 >= dist1[nx][ny]) continue;
+      dist2[nx][ny] = dist2[cur.X][cur.Y] + 1;
+      Q2.emplace(nx, ny);
     }
   }
   cout << "IMPOSSIBLE";
