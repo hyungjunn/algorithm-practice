c9e58e32cccdf1d3fa1ee96c97cabcc231b1d8de
hyungjunn <hotfirebulldog@gmail.com>
2026-02-16 12:29:20 +0900

    백준 2675번: 리팩터링

--- baekjoon/2675.cpp
+++ baekjoon/2675.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int T;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int T, R;
+  string s;
   cin >> T;
   while (T--) {
-    int R;
-    cin >> R;
-    string s;
-    cin >> s;
-    for (const auto &ch : s) {
+    cin >> R >> s;
+    for (const auto ch : s) {
       for (int i = 0; i < R; i++) cout << ch;
     }
     cout << '\n';
