30f6981d77a30fd53019f070d84a10a0841b9d10
hyungjunn <hotfirebulldog@gmail.com>
2026-02-16 13:53:24 +0900

    백준 10809번: 리팩터링

--- baekjoon/10809.cpp
+++ baekjoon/10809.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int alpha[26]; // 알파벳이 처음 등장하는 인덱스
+vector<int> findAlphabetIndices(const string & s) {
+  vector<int> alpha(26, -1);
+  for (size_t i = 0; i < s.length(); ++i) {
+    int charIdx = s[i] - 'a';
+    if (alpha[charIdx] == -1) {
+      alpha[charIdx] = static_cast<int>(i);
+    }
+  }
+  return alpha;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   string S;
-  cin >> S;
-  for (int i = 0; i < 26; i++) alpha[i] = -1;
-  for (int i = 0; i < S.length(); i++) {
-    // i에 해당하는 문자 - 'a'의 alpha에 i를 할당. 단, -1이 아닌 경우 continue;
-    if (alpha[S[i] - 'a'] != -1) continue;
-    alpha[S[i] - 'a'] = i;
-  }
-  for (int i = 0; i < 26; i++) {
-    cout << alpha[i] << ' ';
+  if (cin >> S) {
+    vector<int> result = findAlphabetIndices(S);
+    for (int i = 0; i < result.size(); i++) {
+      cout << result[i] << (i == result.size() - 1 ? "" : " ");
+    }
+    cout << '\n';
   }
   return 0;
 }
